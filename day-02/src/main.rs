cf5a10b02e76c8ec85c9cb46a9032fb044915c22
Guido Knips <guido.knips@posteo.de>
2025-12-02 21:27:34 +0100

    Add solution for day 2, part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -14,6 +14,9 @@ fn main() -> Result<(), String> {
     let invalid_sum = sum_invalid(&ids);
     println!("The sum of invalid IDs is {invalid_sum}.");
 
+    let invalid_sum = sum_invalid_2(&ids);
+    println!("With more silly pattern, the sum of invalid IDs is {invalid_sum}");
+
     Ok(())
 }
 
@@ -23,12 +26,38 @@ fn sum_invalid(ids: &[(u64, u64)]) -> u64 {
         .sum()
 }
 
+fn sum_invalid_2(ids: &[(u64, u64)]) -> u64 {
+    ids.iter()
+        .flat_map(|(from, to)| (*from..=*to).filter(|id| is_invalid_2(*id)))
+        .sum()
+}
+
 fn is_invalid(id: u64) -> bool {
     if id == 0 {
         return false;
     }
     let p = 10u64.pow(id.ilog10().div_ceil(2));
     id % p == id / p
+}
+
+fn is_invalid_2(id: u64) -> bool {
+    if id == 0 {
+        return false;
+    }
+    let len = id.ilog10() + 1;
+    let emax = len / 2;
+    (1..=emax).filter(|e| len.is_multiple_of(*e)).any(|e| {
+        let p = 10u64.pow(e);
+        let seq = id % p;
+        let mut id = id / p;
+        while id != 0 {
+            if id % p != seq {
+                return false;
+            }
+            id /= p;
+        }
+        true
+    })
 }
 
 fn parse(input: &str) -> Result<Vec<(u64, u64)>, String> {
@@ -63,5 +92,17 @@ mod test {
 
         // then
         assert_eq!(sum, 1227775554);
+    }
+
+    #[test]
+    fn sum_invalid_2_works_for_example() {
+        // given
+        let ids = parse(EXAMPLE_INPUT).expect("expected valid input");
+
+        // when
+        let sum = sum_invalid_2(&ids);
+
+        // then
+        assert_eq!(sum, 4174379265);
     }
 }
