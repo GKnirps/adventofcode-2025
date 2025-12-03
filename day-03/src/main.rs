0f2b9be62017a01e2aa156c0acfead9cc3ab33f4
Guido Knips <guido.knips@posteo.de>
2025-12-03 18:31:41 +0100

    Add solution for day 3, part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -11,40 +11,51 @@ fn main() -> Result<(), String> {
     let content = read_to_string(Path::new(&filename)).map_err(|e| e.to_string())?;
     let banks = parse(&content)?;
 
-    let joltage = max_joltage_sum(&banks);
+    let joltage = max_joltage_sum(&banks, 2);
     println!("The total output of joltage is {joltage}.");
+
+    let joltage = max_joltage_sum(&banks, 12);
+    println!("The total output of high joltage is {joltage}.");
 
     Ok(())
 }
 
-fn max_joltage_sum(banks: &[Vec<u32>]) -> u32 {
-    banks.iter().map(|bank| max_joltage_for_bank(bank)).sum()
+fn max_joltage_sum(banks: &[Vec<u64>], n: u8) -> u64 {
+    banks
+        .iter()
+        .map(|bank| max_joltage_for_bank(bank, n, 0))
+        .sum()
 }
 
-fn max_joltage_for_bank(bank: &[u32]) -> u32 {
-    if bank.len() < 2 {
-        return 0;
-    }
-    let mut max_digit_1 = 0;
-    let mut digit_1_pos = 0;
-    for (i, digit) in bank[..bank.len() - 1].iter().enumerate() {
-        if *digit > max_digit_1 {
-            max_digit_1 = *digit;
-            digit_1_pos = i;
+fn max_joltage_for_bank(bank: &[u64], n: u8, current: u64) -> u64 {
+    if bank.len() < n.into() {
+        0
+    } else if n > 1 {
+        let mut max_digit = 0;
+        let mut digit_pos = 0;
+
+        for (i, digit) in bank[..(bank.len() - ((n as usize) - 1))].iter().enumerate() {
+            if *digit > max_digit {
+                max_digit = *digit;
+                digit_pos = i;
+            }
         }
+        max_joltage_for_bank(&bank[digit_pos + 1..], n - 1, current * 10 + max_digit)
+    } else {
+        let max_digit = bank.iter().max().copied().unwrap_or(0);
+        current * 10 + max_digit
     }
-    let max_digit_2 = bank[digit_1_pos + 1..].iter().max().copied().unwrap_or(0);
-    max_digit_1 * 10 + max_digit_2
 }
 
-fn parse(input: &str) -> Result<Vec<Vec<u32>>, String> {
+fn parse(input: &str) -> Result<Vec<Vec<u64>>, String> {
     input.lines().map(parse_bank).collect()
 }
 
-fn parse_bank(line: &str) -> Result<Vec<u32>, String> {
+fn parse_bank(line: &str) -> Result<Vec<u64>, String> {
     line.chars()
         .map(|c| {
             c.to_digit(10)
+                .map(|digit| digit as u64)
                 .ok_or_else(|| format!("invalid joltage: '{c}'"))
         })
         .collect()
